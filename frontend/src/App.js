d71d02b747451544f845abb8666303e4246f981f
lawj4 <jlaw0430@gmail.com>
2024-08-27 16:49:09 -0700

    bye

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+//App.js
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Todo from './components/Todo';
+
+
+function App() {
+  const headStyle = {
+    textAlign: "center",
+  }
+  return (
+    <div>
+      <h1 style={headStyle}>UCI Housing</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Todo/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
